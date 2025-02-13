dfd16190faf8eb463e3876691b662806027fc8f5
konstantin <kostia505@gmail.com>
2025-02-13 12:38:58 +0100

    ntl: test deploy settings

--- netlify/functions/cloudinary-images.ts
+++ netlify/functions/cloudinary-images.ts
@@ -6,6 +6,9 @@ c.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
 
 if (
   !process.env.CLOUDINARY_CLOUD_NAME ||
