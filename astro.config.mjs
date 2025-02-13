c019070f4ecec3aeaef9054b6b38a80132fdb677
konstantin <kostia505@gmail.com>
2025-02-13 11:02:12 +0100

    reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kostia2231.github.io",
-  base: "astro",
   devToolbar: {
     enabled: false,
   },
 
   vite: { plugins: [tailwindcss()] },
   integrations: [react()],
-
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
