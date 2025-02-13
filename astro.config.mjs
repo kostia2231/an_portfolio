9be8a4531ca4b16f3f969d85bf6bc2b6d146070f
konstantin <kostia505@gmail.com>
2025-02-13 18:14:32 +0100

    ntl: test deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
@@ -12,4 +11,6 @@ export default defineConfig({
 
   vite: { plugins: [tailwindcss()] },
   integrations: [react()],
+  output: "server",
+  adapter: netlify(),
 });
